a8594e2ec97901240a15d7afef738ec5502c4c2b
mariusilie70 <marius_ilie@yahoo.com>
2023-12-03 21:13:02 +0200

    Update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "wantsome.ro/messagingapp/internal/server"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"wantsome.ro/messagingapp/internal/server"
+)
 
 func main() {
-	server.RunServer()
+	// Create a new server instance
+	s := server.NewServer()
+
+	// Set up graceful shutdown
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-stop
+		log.Println("Shutting down the server...")
+		s.Close()
+		os.Exit(0)
+	}()
+
+	// Start the server
+	addr := ":8080"
+	log.Printf("Starting server on %s\n", addr)
+	if err := http.ListenAndServe(addr, s.Router()); err != nil {
+		log.Fatal(err)
+	}
 }
