7ed155736dd6a20eb85b9e3586d128fb51433cd8
mariusilie70 <marius_ilie@yahoo.com>
2023-12-07 21:28:28 +0200

    Update server.go

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,62 +1,134 @@
 package server
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/gorilla/websocket"
 )
 
-type Server struct {
-	router *http.ServeMux
+var shutdown os.Signal = syscall.SIGUSR1
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
-func NewServer() *Server {
-	return &Server{
-		router: http.NewServeMux(),
+func RunServer() {
+	http.HandleFunc("/", home)
+	http.HandleFunc("/ws", handleConnections)
+
+	go handleMessages()
+
+	server := &http.Server{Addr: ":8080"}
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
+	go func() {
+		log.Printf("Starting server on %s\n", server.Addr)
+		if err := server.ListenAndServe(); err != nil {
+			log.Printf("error starting server: %s", err)
+			stop <- shutdown
+		}
+	}()
+
+	signal := <-stop
+	log.Printf("Shutting down server ... ")
+
+	m.Lock()
+	for conn := range userConnections {
+		conn.Close()
+		delete(userConnections, conn)
+	}
+	m.Unlock()
+
+	server.Shutdown(nil)
+	if signal == shutdown {
+		os.Exit(1)
 	}
 }
 
-func (s *Server) Router() *http.ServeMux {
-	s.router.HandleFunc("/ws", s.handleWebSocket)
-	return s.router
+func home(w http.ResponseWriter, r *http.Request) {
+	log.Println("Home page requested")
+	fmt.Fprintf(w, "Hello world from my server!")
 }
 
-func (s *Server) handleWebSocket(w http.ResponseWriter, r *http.Request) {
-	upgrader := websocket.Upgrader{
-		CheckOrigin: func(r *http.Request) bool { return true },
-	}
-
+func handleConnections(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Println(err)
+		log.Printf("error upgrading connection: %s", err)
 		return
 	}
 
-	go s.handleConnection(conn)
+	log.Println("New client connected")
+
+	// Register new client connection
+	m.Lock()
+	userConnections[conn] = ""
+	m.Unlock()
+
+	// Handle incoming messages from the client
+	go handleIncomingMessages(conn)
+
+	// Send welcome message to the client
+	welcomeMessage := models.Message{
+		Sender:    "Server",
+		Recipient: "",
+		Content:   "Welcome to the messaging app!",
+	}
+	conn.WriteJSON(welcomeMessage)
 }
 
-func (s *Server) handleConnection(conn *websocket.Conn) {
-	defer conn.Close()
-
+func handleIncomingMessages(conn *websocket.Conn) {
 	for {
-		messageType, message, err := conn.ReadMessage()
+		var message models.Message
+		err := conn.ReadJSON(&message)
 		if err != nil {
-			log.Println(err)
+			log.Printf("error reading message: %s", err)
 			break
 		}
 
-		log.Printf("Received message: %s\n", message)
+		log.Printf("Received message from client: %+v", message)
 
-		err = conn.WriteMessage(messageType, []byte(fmt.Sprintf("Server received message: %s", message)))
-		if err != nil {
-			log.Println(err)
+		if message.Recipient == "" {
+			broadcast <- message
+		} else {
+			sendMessageToRecipient(message)
+		}
+	}
+
+	conn.Close()
+	log.Println("Client disconnected")
+	m.Lock()
+	delete(userConnections, conn)
+	m.Unlock()
+}
+
+func sendMessageToRecipient(message models.Message) {
+	m.Lock()
+	defer m.Unlock()
+
+	for conn := range userConnections {
+		if userConnections[conn] == message.Recipient {
+			conn.WriteJSON(message)
 			break
 		}
 	}
 }
 
-func (s *Server) Close() {
-	// Perform any necessary cleanup here
+func handleMessages() {
+	for {
+		message := <-broadcast
+
+		m.Lock()
+		for conn := range userConnections {
+			conn.WriteJSON(message)
+		}
+		m.Unlock()
+	}
 }
